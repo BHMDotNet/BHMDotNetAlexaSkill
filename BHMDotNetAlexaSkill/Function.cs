7f306178fa0b931a5e75b97ef0706fc70b44dfff
Blake Helms <blake@blakehelms.net>
2017-01-03 08:05:02 -0600

    Initial project creation

--- BHMDotNetAlexaSkill/Function.cs
+++ BHMDotNetAlexaSkill/Function.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization;
+using Slight.Alexa.Framework.Models.Requests;
+using Slight.Alexa.Framework.Models.Responses;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -20,9 +22,44 @@ namespace BHMDotNetAlexaSkill
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            Response response;
+            IOutputSpeech innerResponse = null;
+            var log = context.Logger;
+
+            if (input.GetRequestType() == typeof(Slight.Alexa.Framework.Models.Requests.RequestTypes.ILaunchRequest))
+            {
+                // default launch request, let's just let them know what you can do
+                log.LogLine($"Default LaunchRequest made");
+
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = "Welcome to number functions.  You can ask us to add numbers!";
+            }
+            else if (input.GetRequestType() == typeof(Slight.Alexa.Framework.Models.Requests.RequestTypes.IIntentRequest))
+            {
+                // intent request, process the intent
+                log.LogLine($"Intent Requested {input.Request.Intent.Name}");
+
+                // AddNumbersIntent
+                // get the slots
+                var n1 = Convert.ToDouble(input.Request.Intent.Slots["firstnum"].Value);
+                var n2 = Convert.ToDouble(input.Request.Intent.Slots["secondnum"].Value);
+
+                double result = n1 + n2;
+
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = $"The result is {result.ToString()}.";
+
+            }
+            response = new Response();
+            response.ShouldEndSession = true;
+            response.OutputSpeech = innerResponse;
+            SkillResponse skillResponse = new SkillResponse();
+            skillResponse.Response = response;
+            skillResponse.Version = "1.0";
+
+            return skillResponse;
         }
     }
 }
